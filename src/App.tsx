fcdcfbcdc67417f69ab714b19d19fa51b30f0477
Cameron Morris <cameronjmorris87@gmail.com>
2023-07-27 16:38:48 +1200

    Added side nav bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
+import './styles/App.css'
+import NavBar from "./components/NavBar"
+import { Routes, Route, BrowserRouter} from 'react-router-dom';
+import Home from './pages/Home'
+
 function App() {
 
   return (
     <>
-      <h1>
-        Coming Soon...
-      </h1>
+      <BrowserRouter>
+        <NavBar/>
+          <Routes>
+            <Route path='/' Component={Home}/>
+          </Routes>
+      </BrowserRouter>
     </>
   )
 }
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,44 @@
+
+import { useState } from 'react'
+import * as AiIcons from 'react-icons/ai'
+import * as FaIcons from 'react-icons/fa'
+import { Link } from 'react-router-dom'
+import { NavBarData } from './NavBarData'
+
+
+function NavBar() {
+    const [sidebar, setSidebar] = useState(false)
+
+    const toggleSidebar = () => setSidebar(!sidebar)
+
+    return (
+        <>
+            <div className='navbar'>
+                <Link to='#' className='menu-bars'>
+                    <FaIcons.FaBars onClick={toggleSidebar}/>
+                </Link>
+            </div>
+            <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
+                <ul className='nav-menu-items'>
+                    <li className='navbar-toggle'>
+                        <Link to='#' className='menu-bars'>
+                            <AiIcons.AiOutlineClose onClick={toggleSidebar}/>
+                        </Link>
+                    </li>
+                    {NavBarData.map((item,index) => {
+                        return (
+                            <li key={index} className={item.cName}>
+                                <Link to={item.path}>
+                                    {item.icon}
+                                    <span>{item.title}</span>
+                                </Link>
+                            </li>
+                        )
+                    })}
+                </ul>
+            </nav>
+        </>
+  )
+}
+
+export default NavBar
