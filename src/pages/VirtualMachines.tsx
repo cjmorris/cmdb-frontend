40e33a8bdbb461281d265979f1a950483a96ea3e
Cameron Morris <cameronjmorris87@gmail.com>
2023-07-30 18:00:07 +1200

    Created basic Add VM Page structure

--- src/pages/VirtualMachines.tsx
+++ src/pages/VirtualMachines.tsx
@@ -1,7 +1,49 @@
+import '../styles/virtualMachines.css'
+
 function VirtualMachines() {
     return (
         <>
-            <div className='content'>Virtual Machines coming soon...</div>
+            <div className='content'>
+                <h2>Add Virtual Machine</h2>
+                <div className='vm-grid'>
+                    <div className='vm-grid-item'>
+                        <label className='vm-label'>Hostname</label>
+                        <input className='vm-input'></input>
+                    </div>
+                    <div className='vm-grid-item'>
+                        <label className='vm-label'>Item 1</label>
+                        <input className='vm-input'></input>
+                    </div>
+                    <div className='vm-grid-item'>
+                        <label className='vm-label'>Item 2</label>
+                        <input className='vm-input'></input>
+                    </div>
+                    <div className='vm-grid-item'>
+                        <label className='vm-label'>Item 3</label>
+                        <input className='vm-input'></input>
+                    </div>
+                    <div className='vm-grid-item'>
+                        <label className='vm-label'>Item 4</label>
+                        <input className='vm-input'></input>
+                    </div>
+                    <div className='vm-grid-item'>
+                        <label className='vm-label'>Item 5</label>
+                        <input className='vm-input'></input>
+                    </div>
+                    <div className='vm-grid-item'>
+                        <label className='vm-label'>Item 6</label>
+                        <input className='vm-input'></input>
+                    </div>
+                    <div className='vm-grid-item'>
+                        <label className='vm-label'>Item 7</label>
+                        <input className='vm-input'></input>
+                    </div>
+                    <div className='vm-grid-item'>
+                        <label className='vm-label'>Item 8</label>
+                        <input className='vm-input'></input>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
