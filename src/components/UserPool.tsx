3249614c53edb7ede905640ebc5b88d22354da4c
Cameron Morris <cameronjmorris87@gmail.com>
2023-08-16 17:09:51 +1200

    updated cognito user pool

--- src/components/UserPool.tsx
+++ src/components/UserPool.tsx
@@ -1,8 +1,8 @@
 import { CognitoUserPool } from "amazon-cognito-identity-js";
 
 const poolData = {
-    UserPoolId: "ap-southeast-2_BGUzwAY84",
-    ClientId: "2nq58ijungbuk8lcvdjv6ahmfo"
+    UserPoolId: "ap-southeast-2_MzfU95UX9",
+    ClientId: "6mu97sv48renrkikt44fbhgij8"
 }
 
 export default new CognitoUserPool(poolData)
